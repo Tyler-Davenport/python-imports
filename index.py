7356b6f0be1d6356ca5d8b3ebda3fcca7ff535d4
Tyler Davenport <tydavenport0415@gmail.com>
2025-04-08 18:53:44 -0500

    all errors cleared and can opened

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
-from appliances.kitchen import Dishwasher
-from appliances.laundry import Dryer
-from appliances.laundry import Washer
-from appliances.kitchen.utility import Refrigerator
+from appliances.kitchen.utility.dishwasher import Dishwasher
+from appliances.laundry.dryer import Dryer
+from appliances.laundry.washer import Washer
+from appliances.kitchen.utility.refrigerator import Refrigerator
+from appliances.kitchen.coffeemaker import CoffeeMaker
+from appliances.kitchen.can_opener import CanOpener
 
 whirlpool_dishwasher = Dishwasher("black")
 whirlpool_dishwasher.wash_dishes()
@@ -14,3 +16,6 @@ lg_fridge.make_ice()
 
 mr_coffee = CoffeeMaker("white")
 mr_coffee.make_coffee()
+
+little_canopener = CanOpener("orange")
+little_canopener.open_can()
